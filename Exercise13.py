c301bad15b3b98acbf2d4f58771aa3b9b00024c8
Mhd-Shuvo <mehedishuvo333@gmail.com>
2020-11-01 18:06:54 +0600

    python exercise

---
+++ Exercise13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script = argv
+
+print("The script is called:", script)
+print("Your first variable is:")
+print("Your second variable is:" )
+print("Your third variable is:" )
---
+++ practice3.py
@@ -0,0 +1,17 @@
+Name = 'Mehedi Hasan Shuvo'
+Age = 25
+Height = (65 * 2.54) # centimeter
+Weight = (180 * 0.45) # kilograms
+Eyes = 'Black'
+Teeth = 'White'
+Hair = 'Black'
+
+print(f"Let's talk about {Name}.")
+print(f"He's {Height} centimeters tall.")
+print(f"He's {Weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {Eyes} eyes and {Hair}hair.")
+print(f"His teeth are usually {Teeth} depending on the cofee.")
+
+total = Age + Height + Weight
+print(f"If i add {Age}, {Height}  and  {Weight} I get {total}")
