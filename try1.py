d1aa6d133a4937c716f35586016a52d74001634f
Mhd-Shuvo <mehedishuvo333@gmail.com>
2020-11-04 17:31:05 +0600

    4-11-2020

---
+++ try1.py
@@ -0,0 +1,12 @@
+# i want to print table of 10 using for loop
+
+number = 10
+for i in range (1,11):
+    print (number,'x', i, '=', number * i)
+
+#table of 10 using while loop
+number = 10
+i = 0
+while i < 10:
+    i = i + 1
+    print (number, 'x', i, '=', number*i)
