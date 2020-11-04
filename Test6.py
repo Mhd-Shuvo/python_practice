3e351c050c95e771731e1207c51b932619c392f2
Mhd-Shuvo <mehedishuvo333@gmail.com>
2020-11-04 18:07:38 +0600

    2-11-2020

---
+++ Test6.py
@@ -0,0 +1,15 @@
+mark = 100
+if (mark >= 50 and mark <60):
+    print ("You're selected as a Junior Programmer!")
+
+elif (mark >= 60 and mark < 70):
+    print ("You're selected as a Senior Programmer!")
+
+elif (mark >= 70 and mark < 80):
+    print ("You're selected as a Analyst Programmer!")
+
+elif (mark >= 80 and mark <= 100):
+    print ("You're System Analyst!")
+
+else :
+    print ("Go look for another Job!")
---
+++ test5.py
@@ -0,0 +1,6 @@
+mark = 20
+if mark >= 40:
+    print ("Pass Mark")
+
+else :
+    print ("You Failed!")
