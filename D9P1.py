034ef89b499fcf7834096dfb0500b76d02932ea0
Mhd-Shuvo <mehedishuvo333@gmail.com>
2020-11-09 18:05:51 +0600

    9-11-2020

---
+++ D9P1.py
@@ -0,0 +1,8 @@
+T = int (input())
+for n in range (T):
+    n = n+2
+    if((n%2)==0):
+        print(input() ,'is even')
+
+    elif((n%2)==1):
+        print(input(), 'is odd')
---
+++ D9P2.py
@@ -0,0 +1,5 @@
+for x in range (1000, 0, -1):
+    if (x - 1) % 5 == 0:
+        print(x, end = '\n')
+    else:
+        print(x, end = '\t')
